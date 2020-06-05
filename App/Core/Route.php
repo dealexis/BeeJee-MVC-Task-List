ba87d95dda9a197d3aa1f1e285160e3d67d18147
AlexIS <47022552+dealexis@users.noreply.github.com>
2020-06-05 11:45:32 +0500

    Remove function strtolower in Route.php for fix case sensitivity

--- App/Core/Route.php
+++ App/Core/Route.php
@@ -34,14 +34,14 @@ class Route
         $controller_name = 'Controller'.$controller_name;
         $action_name = 'action_'.$action_name;
 
-        $model_file = strtolower($model_name).'.php';
+        $model_file = $model_name.'.php';
         $model_path = "App/Models/".$model_file;
         if(file_exists($model_path))
         {
             include "App/Models/".$model_file;
         }
 
-        $controller_file = strtolower($controller_name).'.php';
+        $controller_file = $controller_name.'.php';
         $controller_path = "App/Controllers/".$controller_file;
         if(file_exists($controller_path))
         {
